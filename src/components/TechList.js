264baa0cc89e03f48c3db4888641fe679169f625
Mike <m1kros@hotmail.com>
2019-09-08 04:03:24 -0300

    Limpando comentarios

--- src/components/TechList.js
+++ src/components/TechList.js
@@ -1,18 +1,13 @@
-import React, { Component, Fragment } from "react";
+import React, { Component } from "react";
 
 import TechItem from "./TechItem";
 
 class TechList extends Component {
-  //static defaultProps = {
-  //  tech: "Oculto"
-  //};
-
   state = {
     newTech: "",
     techs: []
   };
 
-  // Executado assim que o componente aparece em tela
   componentDidMount() {
     const techs = localStorage.getItem("techs");
 
@@ -21,27 +16,17 @@ class TechList extends Component {
     }
   }
 
-  // Executado sempre que houver alterações nas props ou estado
-  // prevProps, prevState { Valores anteriores das Props e State }
-  // utilizado _ no primeiro parâmetro para indicar que não vamos utilizar
-  // prevProps
   componentDidUpdate(_, prevState) {
     if (prevState.techs !== this.state.techs) {
       localStorage.setItem("techs", JSON.stringify(this.state.techs));
     }
   }
 
-  // Executado quando o componente deixa de existir
-  //componentWillUnmount() {}
-
   handleInputChange = e => {
     this.setState({ newTech: e.target.value });
   };
 
   handleSubmit = e => {
-    // por padrão a funcionalidade padrão de um formulário após um submit é
-    // atualizar a tela, e com e.preventDefault, vamos prevenir o comportamento
-    // padrão desse form, evitando refresh
     e.preventDefault();
 
     this.setState({
@@ -69,9 +54,6 @@ class TechList extends Component {
         <input
           type="text"
           onChange={this.handleInputChange}
-          // Como boa prática, deve ser definido a property value também no input
-          // para garantir que caso o nosso estado newTech for alterado por algum
-          // meio diferente do onChange o mesmo seja atualizado também.
           value={this.state.newTech}
         />
         <button type="submit">Enviar</button>
