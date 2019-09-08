40e33ebe039467d26f0cab10e4603b687c327469
Mike <m1kros@hotmail.com>
2019-09-08 03:46:34 -0300

    Default Props & PropTypes

--- src/components/TechItem.js
+++ src/components/TechItem.js
@@ -1,5 +1,7 @@
 import React from "react";
 
+import PropTypes from "prop-types";
+
 function TechItem({ tech, onDelete }) {
   return (
     <li key={tech}>
@@ -10,5 +12,14 @@ function TechItem({ tech, onDelete }) {
     </li>
   );
 }
+
+TechItem.defaultProps = {
+  tech: "Oculto"
+};
+
+TechItem.propTypes = {
+  tech: PropTypes.string,
+  onDelete: PropTypes.func.isRequired
+};
 
 export default TechItem;
--- src/components/TechList.js
+++ src/components/TechList.js
@@ -3,6 +3,10 @@ import React, { Component, Fragment } from "react";
 import TechItem from "./TechItem";
 
 class TechList extends Component {
+  //static defaultProps = {
+  //  tech: "Oculto"
+  //};
+
   state = {
     newTech: "",
     techs: ["Node.js", "ReactJS", "React Native"]
