f1ce6dffe210dbea60dd09280803651835feee87
Mike <m1kros@hotmail.com>
2019-09-08 03:19:42 -0300

    Estado & Imutabilidade

--- src/components/TechList.js
+++ src/components/TechList.js
@@ -1,17 +1,45 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 
 class TechList extends Component {
   state = {
+    newTech: "",
     techs: ["Node.js", "ReactJS", "React Native"]
+  };
+
+  handleInputChange = e => {
+    this.setState({ newTech: e.target.value });
+  };
+
+  handleSubmit = e => {
+    // por padrão a funcionalidade padrão de um formulário após um submit é
+    // atualizar a tela, e com e.preventDefault, vamos prevenir o comportamento
+    // padrão desse form, evitando refresh
+    e.preventDefault();
+
+    this.setState({
+      techs: [...this.state.techs, this.state.newTech],
+      newTech: ""
+    });
   };
 
   render() {
     return (
-      <ul>
-        <li>Node.js</li>
-        <li>ReactJS</li>
-        <li>React Native</li>
-      </ul>
+      <form onSubmit={this.handleSubmit}>
+        <ul>
+          {this.state.techs.map(tech => (
+            <li key={tech}>{tech}</li>
+          ))}
+        </ul>
+        <input
+          type="text"
+          onChange={this.handleInputChange}
+          // Como boa prática, deve ser definido a property value também no input
+          // para garantir que caso o nosso estado newTech for alterado por algum
+          // meio diferente do onChange o mesmo seja atualizado também.
+          value={this.state.newTech}
+        />
+        <button type="submit">Enviar</button>
+      </form>
     );
   }
 }
